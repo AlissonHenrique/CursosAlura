944d53637ae8318cf7b050b3d7948b47df8de33e
Alisson Henrique <alisson-henri@live.com>
2022-05-16 11:13:55 -0300

    iniciando autrnticaccao

---
+++ angularArquiteturaFormularios/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  constructor(private httpClient: HttpClient) { }
+}
